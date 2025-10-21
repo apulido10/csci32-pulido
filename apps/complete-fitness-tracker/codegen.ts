6d54defca01023e79a037053de438df781a82901
Alexander <Alexanderpulido10@gmail.com>
2025-10-21 16:31:48 -0700

    completed lab

--- apps/complete-fitness-tracker/codegen.ts
+++ apps/complete-fitness-tracker/codegen.ts
@@ -1,7 +1,12 @@
-import type { CodegenConfig } from '@graphql-codegen/cli'
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const isCI = !!process.env.VERCEL || !!process.env.CI
+const schemaSource = isCI
+  ? './schema.graphql'
+  : 'http://localhost:4000/api/graphql'
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:4000/api/graphql',
+  schema: schemaSource,
   documents: ['src/**/*.{ts,tsx,graphql,gql}', '!src/generated/**/*'],
   generates: {
     'schema.graphql': {
