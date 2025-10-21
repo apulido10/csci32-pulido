bbf99deb3fa7cdb71acf9f01461b31df50c4a91c
Alexander <Alexanderpulido10@gmail.com>
2025-10-21 16:15:35 -0700

    Add frontend authentication with React Hook Form

--- apps/complete-fitness-tracker/codegen.ts
+++ apps/complete-fitness-tracker/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: 'http://localhost:4000/api/graphql',
-  documents: ['src/**/*.{ts,tsx,graphql,gql}', '!src/app/generated/**/*'],
+  documents: ['src/**/*.{ts,tsx,graphql,gql}', '!src/generated/**/*'],
   generates: {
     'schema.graphql': {
       plugins: ['schema-ast'],
